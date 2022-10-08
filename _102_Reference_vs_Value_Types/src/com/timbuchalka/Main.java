fa284192dd62f0c104ff685326c45adba9543d78
Özer Yavuzaslan <ozeryavuzaslan@yahoo.com>
2022-10-08 23:23:10 +0300

    Add files via upload

---
+++ _102_Reference_vs_Value_Types/src/com/timbuchalka/Main.java
@@ -0,0 +1,47 @@
+package com.timbuchalka;
+
+public class Main {
+
+    public static void main(String[] args) {
+        /*
+        System.out.println("Enter values: ");
+        int num;
+        num = scanner.nextInt();
+        int[] myIntArray = new int[num];
+        int[] anotherArray = myIntArray;
+
+        for (int i = 0; i < num; i++){
+            System.out.print("Enter a num: ");
+            myIntArray[i] = scanner.nextInt();
+            System.out.println();
+        }
+
+        for (int i = 0; i < myIntArray.length; i++) {
+            System.out.println("MyIntArray: " + myIntArray[i]);
+            System.out.println("AnotherArray: " + anotherArray[i]);
+        }
+
+        Arrays.sort(myIntArray);
+        System.out.println("After SORT");
+
+        for (int i = 0; i < myIntArray.length; i++) {
+            System.out.println("MyIntArray: " + myIntArray[i]);
+            System.out.println("AnotherArray: " + anotherArray[i]);
+        }
+
+        System.out.println(Arrays.toString(myIntArray) + "\n" + Arrays.toString(anotherArray));
+        */
+
+        int[] myIntArray = Library.readIntegers();
+
+        /*
+        int minValue = Library.findMin(myIntArray);
+        System.out.println("My min value: " + minValue);
+        System.out.println("Manually found min value: " + Library.findMinManually(myIntArray));
+        */
+
+        Library.printArray(myIntArray);
+        myIntArray = Library.reverseMyIntArray(myIntArray, Library.IsSortingRequested());
+        Library.printArray(myIntArray);
+    }
+}
