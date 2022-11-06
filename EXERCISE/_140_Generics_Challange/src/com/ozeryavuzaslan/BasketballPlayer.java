55fd0df06b17db79b94e7068539ecf1fb68cf86d
Özer Yavuzaslan <ozeryavuzaslan@yahoo.com>
2022-11-07 00:46:07 +0300

    Delete EXERCISE/_140_Generics_Challange directory

--- EXERCISE/_140_Generics_Challange/src/com/ozeryavuzaslan/BasketballPlayer.java
+++
@@ -1,7 +0,0 @@
-package com.ozeryavuzaslan;
-
-public class BasketballPlayer extends Player{
-    public BasketballPlayer(String name, int age) {
-        super(name, age);
-    }
-}
--- EXERCISE/_140_Generics_Challange/src/com/ozeryavuzaslan/SoccerPlayer.java
+++
@@ -1,7 +0,0 @@
-package com.ozeryavuzaslan;
-
-public class SoccerPlayer extends Player{
-    public SoccerPlayer(String name, int age) {
-        super(name, age);
-    }
-}
