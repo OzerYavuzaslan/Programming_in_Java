a0d7be63bdac86132cea48775052109e94c237e3
Özer Yavuzaslan <ozeryavuzaslan@yahoo.com>
2022-11-30 07:57:16 +0300

    Delete BOOTCAMP/_1_1_Palindromik directory

--- BOOTCAMP/_1_1_Palindromik/src/com/ozeryavuzaslan/Main.java
+++
@@ -1,9 +0,0 @@
-package com.ozeryavuzaslan;
-
-public class Main {
-    public static void main(String[] args) {
-        for (int i = 10; i <= 100; i++){
-            Calculation.findPalindromicNumber(i);
-        }
-    }
-}
