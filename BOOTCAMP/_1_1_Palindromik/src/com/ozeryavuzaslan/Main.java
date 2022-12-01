db66be546b692b274fed7d1d7ca19a260db01fc3
Özer Yavuzaslan <ozeryavuzaslan@yahoo.com>
2022-12-01 23:42:51 +0300

    Update Main.java

--- BOOTCAMP/_1_1_Palindromik/src/com/ozeryavuzaslan/Main.java
+++ BOOTCAMP/_1_1_Palindromik/src/com/ozeryavuzaslan/Main.java
@@ -2,8 +2,7 @@ package com.ozeryavuzaslan;
 
 public class Main {
     public static void main(String[] args) {
-        for (int i = 10; i <= 100; i++){
+        for (int i = 10; i <= 100; i++)
             Calculation.findPalindromicNumber(i);
-        }
     }
 }
