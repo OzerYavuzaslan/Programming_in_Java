145482972f57377fe301a846049796c689943cf9
Özer Yavuzaslan <ozeryavuzaslan@yahoo.com>
2023-08-20 13:50:29 +0300

    Add files via upload

---
+++ EmployeManagementProject/config-server/src/test/java/com/ozeryavuzaslan/configserver/ConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ozeryavuzaslan.configserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
