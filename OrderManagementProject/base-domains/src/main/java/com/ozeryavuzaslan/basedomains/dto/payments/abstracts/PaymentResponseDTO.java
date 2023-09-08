50a186fc214970913cc56c252e28116fac548317
Özer Yavuzaslan <ozeryavuzaslan@yahoo.com>
2023-09-09 01:25:29 +0300

    Ongoing project

--- OrderManagementProject/base-domains/src/main/java/com/ozeryavuzaslan/basedomains/dto/payments/abstracts/PaymentResponseDTO.java
+++ OrderManagementProject/base-domains/src/main/java/com/ozeryavuzaslan/basedomains/dto/payments/abstracts/PaymentResponseDTO.java
@@ -28,5 +28,5 @@ public abstract class PaymentResponseDTO {
     private LocalDateTime paymentDate;
 
     @Builder.Default
-    private String someText = "someText";
+    private String tmpStr = "STRRRRRRRR";
 }
