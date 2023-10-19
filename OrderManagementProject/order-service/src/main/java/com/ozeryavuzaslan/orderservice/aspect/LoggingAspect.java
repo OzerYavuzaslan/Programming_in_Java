9d7e91fb9da906d9f4e8430593cf7fd87457e4b8
Özer Yavuzaslan <ozeryavuzaslan@yahoo.com>
2023-10-19 20:21:14 +0300

    Ongoing project

--- OrderManagementProject/order-service/src/main/java/com/ozeryavuzaslan/orderservice/aspect/LoggingAspect.java
+++ OrderManagementProject/order-service/src/main/java/com/ozeryavuzaslan/orderservice/aspect/LoggingAspect.java
@@ -1,16 +1,13 @@
 package com.ozeryavuzaslan.orderservice.aspect;
 
+import com.ozeryavuzaslan.basedomains.util.ColorConstants;
 import lombok.extern.slf4j.Slf4j;
 import org.aspectj.lang.JoinPoint;
 import org.aspectj.lang.ProceedingJoinPoint;
-import org.aspectj.lang.annotation.AfterThrowing;
-import org.aspectj.lang.annotation.Around;
-import org.aspectj.lang.annotation.Aspect;
-import org.aspectj.lang.annotation.Pointcut;
+import org.aspectj.lang.annotation.*;
 import org.springframework.stereotype.Component;
 
 import java.util.Arrays;
-
 
 /**
  * springBeanPointcut() - Pointcut that matches all repositories, services, and Web REST endpoints.
@@ -23,7 +20,7 @@ import java.util.Arrays;
 @Component
 public class LoggingAspect {
     @Pointcut("within(@org.springframework.stereotype.Repository *)" +
-            "within(@org.springframework.stereotype.Component *)" +
+            " || within(@org.springframework.stereotype.Component *)" +
             " || within(@org.springframework.stereotype.Service *)" +
             " || within(@org.springframework.web.bind.annotation.RestController *)")
     public void springBeanPointcut() {
@@ -46,27 +43,48 @@ public class LoggingAspect {
 
     @AfterThrowing(pointcut = "applicationPackagePointcut() && springBeanPointcut()", throwing = "e")
     public void logAfterThrowing(JoinPoint joinPoint, Throwable e) {
-        log.error("Exception in {}.{}() with cause = {}", joinPoint.getSignature().getDeclaringTypeName(),
-                joinPoint.getSignature().getName(), e.getCause() != null ? e.getCause() : "NULL");
+        log.error(ColorConstants.ANSI_RED + "Exception in {}.{}() with cause = {}" + ColorConstants.ANSI_RESET,
+                joinPoint.getSignature().getDeclaringTypeName(),
+                joinPoint.getSignature().getName(),
+                e.getCause() != null ? e.getCause() : "NULL");
     }
 
     @Around("applicationPackagePointcut() && springBeanPointcut()")
     public Object logAround(ProceedingJoinPoint joinPoint) throws Throwable {
         if (log.isDebugEnabled()) {
-            log.debug("Enter: {}.{}() with argument[s] = {}", joinPoint.getSignature().getDeclaringTypeName(),
-                    joinPoint.getSignature().getName(), Arrays.toString(joinPoint.getArgs()));
+            String message = ColorConstants.ANSI_GREEN
+                    + String.format("Enter: %s.%s() with argument[s] = %s",
+                    joinPoint.getSignature().getDeclaringTypeName(),
+                    joinPoint.getSignature().getName(),
+                    Arrays.toString(joinPoint.getArgs()))
+                    + ColorConstants.ANSI_RESET;
+
+            log.debug(message);
         }
 
         try {
             Object result = joinPoint.proceed();
+
             if (log.isDebugEnabled()) {
-                log.debug("Exit: {}.{}() with result = {}", joinPoint.getSignature().getDeclaringTypeName(),
-                        joinPoint.getSignature().getName(), result);
+                String exitMessage = ColorConstants.ANSI_YELLOW
+                        + String.format("Exit: %s.%s() with result = %s",
+                        joinPoint.getSignature().getDeclaringTypeName(),
+                        joinPoint.getSignature().getName(),
+                        result)
+                        + ColorConstants.ANSI_RESET;
+
+                log.debug(exitMessage);
             }
+
             return result;
-        } catch (IllegalArgumentException e) {
-            log.error("Illegal argument: {} in {}.{}()", Arrays.toString(joinPoint.getArgs()),
-                    joinPoint.getSignature().getDeclaringTypeName(), joinPoint.getSignature().getName());
+        } catch (Exception e) {
+            String errorMessage = ColorConstants.ANSI_RED
+                    + String.format("Exception: %s in %s.%s()",
+                    Arrays.toString(joinPoint.getArgs()),
+                    joinPoint.getSignature().getDeclaringTypeName(),
+                    joinPoint.getSignature().getName())
+                    + ColorConstants.ANSI_RESET;
+            log.error(errorMessage);
             throw e;
         }
     }
